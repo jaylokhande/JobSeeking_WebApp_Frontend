e3e69338b09fda84438a77cb885c5deaab6b9764
jay <jaylokhande5727@gmail.com>
2024-05-06 19:00:49 +0530

    Job Details Feture added

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ import Login from './components/Auth/Login'
 import Register from './components/Auth/Register'
 import Navbar from './components/Layout/Navbar'
 import Footer from './components/Layout/Footer'
-// import Jobs from './components/Job/Jobs'
+import Jobs from './components/Job/Jobs'
 import Home from './components/Home/Home'
-// import JobDetails from './components/Job/JobDetails'
+import JobDetails from './components/Job/JobDetails'
 // import PostJob from './components/Job/PostJob'
 // import MyJobs from './components/Job/MyJobs'
 // import Application from './components/Application/Application'
 // import MyApplication from './components/Application/MyApplication'
-// import NotFound from './components/NotFound/NotFound'
+import NotFound from './components/NotFound/NotFound'
 import axios from 'axios'
 import { Toaster } from 'react-hot-toast'
 
@@ -49,13 +49,13 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/" element={<Home />} />
-          {/* <Route path="/job/getall" element={<Jobs />} />
+          <Route path="/job/getall" element={<Jobs />} />
           <Route path="/job/:id" element={<JobDetails />} />
-          <Route path="/application/:id" element={<Application />} />
+          {/* <Route path="/application/:id" element={<Application />} />
           <Route path="/applications/me" element={<MyApplications />} />
           <Route path="/job/post" element={<PostJob />} />
-          <Route path="/job/me" element={<MyJobs />} />
-          <Route path="*" element={<NotFound />} /> */}
+          <Route path="/job/me" element={<MyJobs />} /> */}
+          <Route path="/notfound" element={<NotFound />} />
         </Routes>
         <Footer />
         <Toaster />
